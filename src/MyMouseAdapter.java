9dfdc262c225caa79051ad2759102c38db1d6169
Magda Toledo <Magda Toledo@DESKTOP-DRJ6C4N>
2016-10-08 18:44:53 -0400

    fixed minor error on line 68

--- src/MyMouseAdapter.java
+++ src/MyMouseAdapter.java
@@ -65,7 +65,7 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.y = y;
 			int gridX = myPanel.getGridX(x, y);
 			int gridY = myPanel.getGridY(x, y);
-			if ((myPanel.mouseDownGridX == -2) || (myPanel.mouseDownGridY == -1)) {
+			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
 			} else {
